185c8fd1a8062c780466e28e599f1272236173e6
Maytham Fahmi <9260645+maythamfahmi@users.noreply.github.com>
2023-11-24 20:55:07 +0100

    Adding key vault

--- WeatherApp.AppHost/Program.cs
+++ WeatherApp.AppHost/Program.cs
@@ -4,10 +4,12 @@ using Microsoft.Extensions.Azure;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedisContainer("rediscache");
+var kv = builder.AddAzureKeyVault("kv");
 
 var appInsightsConnectionString = builder.Configuration["APPLICATIONSIGHTS_CONNECTION_STRING"];
 
 var weatherapi = builder.AddProject<Projects.WeatherApp_Api>("weatherapi")
+    .WithReference(kv)
     .WithEnvironment("APPLICATIONSIGHTS_CONNECTION_STRING", appInsightsConnectionString);
 
 builder.AddProject<Projects.WeatherApp_Web>("frontend")
