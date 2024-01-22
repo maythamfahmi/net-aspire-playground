3f99959f3c4294cf64935b1111a8801ae879b3d1
Maytham Fahmi <9260645+maythamfahmi@users.noreply.github.com>
2024-01-22 20:33:30 +0100

    version update

--- WeatherApp.AppHost/Program.cs
+++ WeatherApp.AppHost/Program.cs
@@ -1,17 +1,14 @@
-using Aspire.Hosting;
-using Microsoft.Extensions.Azure;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedisContainer("rediscache");
 
 var appInsightsConnectionString = builder.Configuration["APPLICATIONSIGHTS_CONNECTION_STRING"];
 
-var weatherapi = builder.AddProject<Projects.WeatherApp_Api>("weatherapi")
+var weatherApi = builder.AddProject<Projects.WeatherApp_Api>("weatherapi")
     .WithEnvironment("APPLICATIONSIGHTS_CONNECTION_STRING", appInsightsConnectionString);
 
 builder.AddProject<Projects.WeatherApp_Web>("frontend")
-    .WithReference(weatherapi)
+    .WithReference(weatherApi)
     .WithReference(cache)
     .WithEnvironment("APPLICATIONSIGHTS_CONNECTION_STRING", appInsightsConnectionString);
 
